18f4aa0c9631840819f29a85d6bd51fcf406b59b
alumno <alumno@a51-03.local>
2017-04-06 21:12:53 -0500

    ubicación obtenemos longitud y latitud y pide permiso para utilizar la ubicación

--- Core Location en IOS/ViewController.swift
+++ Core Location en IOS/ViewController.swift
@@ -7,12 +7,20 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    let manager = CLLocationManager();
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //cada cuanto va a tomar la ubicacion mientras mas lejos menos bateria consume
+        manager.desiredAccuracy = kCLLocationAccuracyThreeKilometers
+        manager.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func btnActivarTouch(_ sender: UIButton) {
+        
+        if CLLocationManager.authorizationStatus() != .authorizedAlways{
+            
+            manager.requestAlwaysAuthorization();
+            
+        }
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        if status == .authorizedAlways || status == .authorizedWhenInUse{
+            manager.startUpdatingLocation()
+        }
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let ubicacion = locations[0]
+        let coordenadas = ubicacion.coordinate
+        
+        textView.text = textView.text + "\n [\(coordenadas.latitude), \(coordenadas.longitude)]  "
+        
+        //print("\(coordenadas.latitude)", "\(coordenadas.longitude)")
+        
+        
+    }
+    
 }
 
